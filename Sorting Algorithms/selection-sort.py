297697913d829f0b9b27e45eebc89c93675ae38c
geetamatkar <geetamatkar1@gmail.com>
2023-07-14 10:10:52 -0500

    # Complexity analysis

--- Sorting Algorithms/selection-sort.py
+++ Sorting Algorithms/selection-sort.py
@@ -12,3 +12,15 @@ def selection_sort(arr):
 
 arr = [5,8,2,45,21,4]
 print("Sorted array: ", selection_sort(arr))
+
+'''
+Time Comlpexity :
+    The time complexity of selection sort is O(n^2), where n is the number of elements in the array.
+    In the worst-case scenario, selection sort requires two nested loops. O(n^2)
+    The best-case time complexity of selection sort is also O(n^2). Even if the input array is already sorted, the algorithm will still 
+    perform the same number of iterations as in the worst case because it needs to check all elements to confirm their relative order.
+
+
+Space Complexity : 
+    The space complexity of selection sort is O(1), which means it uses a constant amount of additional space regardless of the input size.
+'''
